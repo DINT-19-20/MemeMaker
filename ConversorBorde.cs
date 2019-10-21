6bf541a8f01a9ce6134a0d641b4ea18b6c23cb14
Javier Catalá <javicatala@iesdoctorbalmis.com>
2019-10-21 11:02:34 +0200

    Actualizado el título de la ventana

--- ConversorBorde.cs
+++ ConversorBorde.cs
@@ -12,7 +12,7 @@ namespace MemeMaker
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return ((bool)value ? 1: 0);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
